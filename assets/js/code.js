fd7a87fa5f634f650078713d6a56cc963c62dd2f
anthoox <kray.a4@gmail.com>
2024-02-17 14:50:57 +0100

    optimizando js

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,149 +1,151 @@
 window.onload = function () {
-    /*
-     * Estilo cabecera flotante, boton y nav
-     */
-
-
+    // Variables seleccionadas del DOM
     const header = document.querySelector('header');
     const main = document.querySelector('main');
-    const footer = document.querySelector('footer')
+    const footer = document.querySelector('footer');
     const btnUP = document.querySelector('.btn-up');
-    const nav = document.querySelector('.nav')
-
+    const nav = document.querySelector('.nav');
     const btnMenu = document.querySelector('.icon-menu');
     const btnCerrar = document.querySelector('.icon-cerrar');
-    const btnsMenu = document.querySelectorAll('.icon-nav');
 
-
-
-    var rect = nav.getBoundingClientRect();
-    var posY = rect.top;
-
-    if (posY != 70) {
-
-        header.classList.remove("fixeder_header");
-        main.style.top = '0';
-
-        btnUP.style.display = "block";
-        header.classList.add("fixeder_header")
-        let topHeader = header.offsetHeight;
-        nav.style.top = topHeader + 'px';
-        main.style.top = topHeader + 'px';
-
-    }
-    else {
-        header.classList.remove("fixeder_header");
-        header.classList.remove("border-shadow")
-        main.style.top = '0';
-
-    }
-
+    // Ajustamos los estilos al cargar la página y al cambiar el tamaño de la ventana
+    ajustarEstilo();
     window.addEventListener('resize', ajustarEstilo);
 
+    // Función para ajustar los estilos según el tamaño de la ventana y la posición del nav
     function ajustarEstilo() {
-        var anchoVentana = window.innerWidth;
+        // Obtenemos el ancho de la ventana
+        const anchoVentana = window.innerWidth;
+        // Obtenemos la posición vertical del header
+        const rect = header.getBoundingClientRect();
+        const posY = rect.top;
 
+        // Si el header no está en la posición inicial
+        if (posY !== 0) {
+            // Se ajustan los estilos para fijar el header en la parte superior y mostrar el botón de retorno
+            header.classList.remove("fixeder_header");
+            main.style.top = '0';
+            btnUP.style.display = "block";
+            header.classList.add("fixeder_header");
+            header.classList.add("border-shadow");
+            let topHeader = header.offsetHeight;
+            nav.style.top = topHeader + 'px';
+            main.style.top = topHeader + 'px';
+        } else {
+            // Si el header está en la posición inicial, se eliminan los estilos adicionales
+            header.classList.remove("fixeder_header");
+            header.classList.remove("border-shadow");
+            main.style.top = '0';
+        }
 
-
+        // Si el ancho de la ventana es mayor que 992px
         if (anchoVentana > 992) {
-            nav.style.display = 'none';
-            btnMenu.style.display = 'none';
-            btnCerrar.style.display = 'none';
-            main.style.top = 0;
+            // Se oculta el menú y se establecen otros estilos
+            ocultarMenu();
+            main.style.top = '0';
             footer.style.top = '50px';
             header.style.borderBottom = 'none';
-
-
+            btnCerrar.style.display = 'none';
         } else {
-
+            // Si el ancho de la ventana es menor o igual a 992px, se muestra el botón de menú
             btnMenu.style.display = 'block';
-
-        }
-        if (anchoVentana < 992) {
-            if (btnCerrar.style.display == 'block') {
+            // Si el botón de cerrar está visible, se oculta el botón de menú
+            if (btnCerrar.style.display === 'block') {
                 btnMenu.style.display = 'none';
-                btnCerrar.style.display = 'block';
             }
         }
-
     }
 
+    // Abrir el menú
+    btnMenu.addEventListener('click', abrirMenu);
 
-    btnMenu.addEventListener("click", () => {
+    // Cerrar el menú
+    btnCerrar.addEventListener('click', cerrarMenu);
+
+    // Escuchar el evento de desplazamiento de la ventana
+    window.addEventListener('scroll', () => {
+        // Si el desplazamiento vertical de la ventana es mayor que 0
+        if (window.scrollY > 0) {
+            // Se muestra el botón de retorno y se ajusta el header
+            btnUP.style.display = 'block';
+            ajustarHeader();
+        } else {
+            // Si el desplazamiento vertical de la ventana es 0, se oculta el botón de retorno y se restablece el header
+            btnUP.style.display = 'none';
+            resetearHeader();
+        }
+    });
+
+    // Función para ajustar el header al desplazar la página
+    function ajustarHeader() {
+        // Se obtiene el estado del header
+        const estado = window.getComputedStyle(header).getPropertyValue('position');
+        // Se añade la clase para fijar el header si aún no está fijo
+        header.classList.add('fixeder_header');
+        // Si el header no está fijo, se añade sombra y se ajusta la posición del nav
+        if (estado !== 'fixed') {
+            header.classList.add('border-shadow');
+            const topHeader = header.offsetHeight;
+            nav.style.top = `${topHeader}px`;
+            main.style.top = `${topHeader}px`;
+        }
+    }
+
+    // Función para resetear el header a su estado original
+    function resetearHeader() {
+        // Se obtiene el estado del header
+        const estado = window.getComputedStyle(header).getPropertyValue('position');
+        // Se elimina la clase para fijar el header
+        header.classList.remove('fixeder_header');
+        // Si el header está fijo, se elimina la sombra y se restablecen los estilos
+        if (estado === 'fixed') {
+            header.classList.remove('border-shadow');
+            main.style.top = '0px';
+            header.style.borderBottom = 'none';
+            const rect = nav.getBoundingClientRect();
+            const posY = rect.top;
+            // Si la posición vertical del nav es mayor o igual a 70, se restablecen los estilos
+            if (posY >= 70) {
+                main.style.top = '0px';
+            }
+        }
+    }
+
+    // Función para abrir el menú
+    function abrirMenu() {
         btnMenu.style.display = 'none';
         btnCerrar.style.display = 'block';
         nav.style.display = 'flex';
         nav.style.transform = 'translateY(0%)';
-        let estado = window.getComputedStyle(header).getPropertyValue('position');
-
-        if (estado != 'fixed') {
-            header.classList.add("border-shadow");
-
+        const estado = window.getComputedStyle(header).getPropertyValue('position');
+        // Si el header no está fijo, se añade sombra
+        if (estado !== 'fixed') {
+            header.classList.add('border-shadow');
         }
         header.style.borderBottom = '1px solid var(--cyan)';
-    })
+    }
 
-    btnCerrar.addEventListener("click", () => {
+    // Función para cerrar el menú
+    function cerrarMenu() {
         btnMenu.style.display = 'block';
         btnCerrar.style.display = 'none';
         nav.style.transform = 'translateY(-200%)';
-        let estado = window.getComputedStyle(header).getPropertyValue('position');
-
-        if (estado != 'fixed') {
-            header.classList.remove("border-shadow")
+        const estado = window.getComputedStyle(header).getPropertyValue('position');
+        // Si el header no está fijo, se elimina la sombra después de un pequeño retraso
+        if (estado !== 'fixed') {
+            header.classList.remove('border-shadow');
         }
         setTimeout(() => {
             header.style.borderBottom = '1px solid var(--bg)';
+        }, 325);
+    }
 
-        }, 325)
-    })
-
-    window.addEventListener("scroll", () => {
-
-        let scrollDesk = 0;
-        if (window.scrollY > scrollDesk) {
-            btnUP.style.display = "block";
-
-            let estado = window.getComputedStyle(header).getPropertyValue('position');
-
-            if (estado == 'fixed') {
-
-                header.classList.add("fixeder_header")
-                header.classList.add("border-shadow")
-                let topHeader = header.offsetHeight;
-                nav.style.top = topHeader + 'px';
-                main.style.top = topHeader + 'px';
-
-            } else {
-                header.classList.add("fixeder_header")
-                header.classList.add("border-shadow")
-                let topHeader = header.offsetHeight;
-                main.style.top = topHeader + 'px';
-            }
-
-
-        }
-        else {
-            btnUP.style.display = "none";
-            let estado = window.getComputedStyle(header).getPropertyValue('position');
-            var rect = nav.getBoundingClientRect();
-
-            var posY = rect.top;
-
-            if (posY >= 70) {
-
-                header.classList.remove("fixeder_header");
-                main.style.top = '0px';
-            }
-            else {
-                header.classList.remove("fixeder_header");
-                header.classList.remove("border-shadow")
-                main.style.top = '0px';
-                console.log('sad')
-            }
-
-
-        }
-
-    })
-}
+    // Función para ocultar el menú y restablecer los estilos
+    function ocultarMenu() {
+        btnMenu.style.display = 'none';
+        nav.style.display = 'none';
+        main.style.top = '0';
+        footer.style.top = '50px';
+        header.style.borderBottom = 'none';
+    }
+};
